5ca53686e070691e2bc7e27c89c4de91560fbbb7
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-23 14:59:53 +0530

    add comments

--- Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/GroupConsecutiveElements.swift
+++ Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/GroupConsecutiveElements.swift
@@ -11,9 +11,11 @@ func groupElements(_ elements: [Int]) -> [[Int]] {
     // If elements is empty then you return
     guard !elements.isEmpty else { return [] }
     
+    // First group made of first element
     var currentGroup = [Int]()
     currentGroup.append(elements[0])
     
+    // Main group hold subgroups
     var groups = [[Int]]()
     
     for element in elements.dropFirst() {
@@ -25,7 +27,9 @@ func groupElements(_ elements: [Int]) -> [[Int]] {
         }
     }
 
-    // If the element contains only one or for the last group of the last element
+    // If there is single element or
+    // If last element is different or
+    // if all elements are identical so does not move to else block
     groups.append(currentGroup)
     
     return groups
@@ -43,5 +47,9 @@ final class GroupConsecutiveElements: XCTestCase {
     
     func test_groupElements_deliversTwoGroups_onTwoElementsList() {
         XCTAssertEqual(groupElements([1,2]), [[1], [2]])
+    }
+    
+    func test_groupElements_deliversSingleGroup_onTwoIdenticalElementsList() {
+        XCTAssertEqual(groupElements([1,1]), [[1,1]])
     }
 }
