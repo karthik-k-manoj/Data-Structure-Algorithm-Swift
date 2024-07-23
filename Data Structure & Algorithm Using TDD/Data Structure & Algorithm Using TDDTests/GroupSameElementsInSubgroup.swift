281f4d8d2fee0f33838a968c0d3ab44d1d082398
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-23 14:49:02 +0530

    test groupElement delivers separate groups for each element in the input list

--- Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/GroupSameElementsInSubgroup.swift
+++ Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/GroupSameElementsInSubgroup.swift
@@ -10,7 +10,23 @@ import XCTest
 func groupElements(_ elements: [Int]) -> [[Int]] {
     guard !elements.isEmpty else { return [] }
     
-    return [[elements[0]]]
+    var currentGroup = [Int]()
+    currentGroup.append(elements[0])
+    
+    var groups = [[Int]]()
+    
+    for element in elements.dropFirst() {
+        if currentGroup[currentGroup.startIndex] == element {
+            currentGroup.append(element)
+        } else {
+            groups.append(currentGroup)
+            currentGroup = [element]
+        }
+    }
+
+    groups.append(currentGroup)
+    
+    return groups
 }
 
 
@@ -25,5 +41,11 @@ final class GroupSameElementsInSubgroup: XCTestCase {
         let output = groupElements([1])
         
         XCTAssertEqual(output, [[1]])
+    }
+    
+    func test_groupELements_deliversTwoGroups_onTwoElementsInputList() {
+        let output = groupElements([1,2])
+        
+        XCTAssertEqual(output, [[1], [2]])
     }
 }
