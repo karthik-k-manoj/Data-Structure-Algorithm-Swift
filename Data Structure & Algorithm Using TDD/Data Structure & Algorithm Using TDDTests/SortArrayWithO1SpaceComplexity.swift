6bc37aef1e7179818ef328809f7fa4a725e0389f
Karthik K Manoj <karthikkmanoj@icloud.com>
2024-07-25 10:40:10 +0530

    test delivers 2 elements sorted array on 2 elements sorted array

--- Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/SortArrayWithO1SpaceComplexity.swift
+++ Data Structure & Algorithm Using TDD/Data Structure & Algorithm Using TDDTests/SortArrayWithO1SpaceComplexity.swift
@@ -8,12 +8,24 @@
 import XCTest
 
 func sortWithO1Space(_ array: [Int]) -> [Int] {
-    []
+    guard !array.isEmpty else { return [] }
+
+    return array
 }
 
 final class SortArrayWithO1SpaceComplexity: XCTestCase {
     func test_sort_deliversEmptyArray_onEmptyArray() {
         let output = sortWithO1Space([])
         XCTAssertEqual(output, [])
+    }
+    
+    func test_sort_deliversSingleElementArray_onSingleElementArray() {
+        let output = sortWithO1Space([1])
+        XCTAssertEqual(output, [1])
+    }
+    
+    func test_sort_deliversSorted2ElementsArray_onTwoElementsSortedArray() {
+        let output = sortWithO1Space([1, 2])
+        XCTAssertEqual(output, [1, 2])
     }
 }
